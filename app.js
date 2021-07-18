27dc3353d3aa0356f0b58cf7219c54952fa0fb1f
Jimena <jimena.witen@gmail.com>
2021-07-17 21:33:52 -0300

    linkeando rutas

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ let puerto = 3030;
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
-app.get('login/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('cart/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
-app.get('detalle-de-producto/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
-app.get('register/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/cart', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
+app.get('/detalle-de-producto', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
 app.listen(puerto, () => console.log('Servidor corriendo en http://localhost:' + puerto));
