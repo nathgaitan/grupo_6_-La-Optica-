df69b1b4d079bc71531fc842a7679b4ecbdcec98
leovega06 <leoo_0690@hotmail.com>
2021-08-18 18:03:23 -0300

    agregando login y register ejs

--- site/src/routes/users.js
+++ site/src/routes/users.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {register,login} = require ("../controllers/usersController")
 
+/* GET users listing. */
+router.get("/register",register);
+
+router.get("/login",login);
+
+
+
+module.exports = router;
