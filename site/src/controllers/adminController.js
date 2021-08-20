fa805decaeafa45de9238dae94ce541a43833449
nathgaitan <nathalia2912dante@gmail.com>
2021-08-20 15:57:48 -0300

    agrega crear y editar productos +ruteo

--- site/src/controllers/adminController.js
+++ site/src/controllers/adminController.js
@@ -44,16 +44,16 @@ module.exports = {
         const { name , marca, price, discount, detail } = req.body;
         let product = {
             id: products[products.length-1].id+1,
-            name:+name,
-            marca:+marca,
+            name:name,
+            marca:marca,
             image,
             price: +price,
             discount: +discount,
-            color :+color,
-            detail:+detail,
-            codigo :+codigo,
-            lente : +lente,
-            marco : +marco,
+            color :color,
+            detail:detail,
+            codigo :codigo,
+            lente : lente,
+            marco : marco,
         }
         products.push(product)
         guardar(products)
