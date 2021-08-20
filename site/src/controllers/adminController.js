cf79c70e343e7a37b65c722b5e797f68537de1fb
jimenarw <jimena.witen@gmail.com>
2021-08-20 12:54:53 -0300

    enrutado y controller admin - func solOFtalmoAdd/store, products_db -func guardar genérica

---
+++ site/src/controllers/adminController.js
@@ -0,0 +1,32 @@
+const { products, guardar } = require('../data/products_db');
+
+/* utils */
+let toThousand = require('../utils/toThousand');
+let priceFinal = require('../utils/priceFinal');
+
+module.exports = {
+    solOftalAdd: (req, res) => {
+        res.render('admin/solOftalmoAdd', {
+            title: "Cargar producto"
+        })
+    },
+    solOftalStore: (req, res) => {
+        const { name , marca, price, discount, detail } = req.body;
+        let product = {
+            id: products[products.length - 1].id + 1,
+            name,
+            marca,
+            image : null,
+            price: +price,
+            discount: +discount,
+            color : null,
+            detail,
+            codigo : null,
+            lente : null,
+            marco : null,
+        }
+        products.push(product)
+        guardar(products)
+        return res.redirect('/products')
+    } 
+}
