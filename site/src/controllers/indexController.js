43369fdfa71b18445d8d0231f379297b1f55d928
jimenarw <jimena.witen@gmail.com>
2021-08-20 11:01:15 -0300

    parsear products.json y carrousel de index con glider

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -1,4 +1,6 @@
 const { products } = require('../data/products_db');
+const marcas = require('../data/marcas');
+const galeria = require('../data/galeria');
 
 let toThousand = require('../utils/toThousand');
 let priceFinal = require('../utils/priceFinal');
@@ -8,6 +10,8 @@ module.exports = {
         return res.render('index', {
             title : "Home",
             products,
+            marcas,
+            galeria,
             priceFinal,
             toThousand
         });
