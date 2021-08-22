7abc6df077c8bb1d0a847cf086c0c2c2d2f651cf
jimenarw <jimena.witen@gmail.com>
2021-08-22 20:12:25 -0300

    update terminado

--- site/src/app.js
+++ site/src/app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var adminRouter = require('./routes/admin');
-app.use(methodOverride('_method'));
+
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
