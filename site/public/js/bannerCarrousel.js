f0dee1a41a3b25d31aa208ddd006d6584dbd6d2b
jimenarw <jimena.witen@gmail.com>
2021-11-01 05:29:22 -0300

    validacion back y frond end de login

--- site/public/js/bannerCarrousel.js
+++ site/public/js/bannerCarrousel.js
@@ -1,11 +1,14 @@
-new Glider(document.querySelector('.carousel-banner'), {
-    // Mobile-first defaults
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    scrollLock: true,
-    dots: '.b-dots',
-    arrows: {
-      prev: '.banner-prev',
-      next: '.banner-next'
-    }
-  });
+if(document.querySelector('body.home') || document.querySelector('body.bannerList')) {
+  new Glider(document.querySelector('.carousel-banner'), {
+      // Mobile-first defaults
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      scrollLock: true,
+      dots: '.b-dots',
+      arrows: {
+        prev: '.banner-prev',
+        next: '.banner-next'
+      }
+    });
+
+}
