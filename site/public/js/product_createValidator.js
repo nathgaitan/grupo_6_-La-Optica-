223e9d841c6afaeb28ee759a729f7ed0c06325ce
leovega06 <leoo_0690@hotmail.com>
2021-11-05 00:55:05 -0300

    cambios en productValidator

--- site/public/js/product_createValidator.js
+++ site/public/js/product_createValidator.js
@@ -1,19 +1,71 @@
 console.log('product_createValidator.js success');
 
-let detail = $('detail');
 
-window.addEventListener('keypress', () => {
+$('name').addEventListener('keypress', () => {
     switch (true) {
-        case detail.value.length < 10:
-            detail.classList.add('is-invalid')
-            $('error-detail').innerText = "El detalle debe tener un mínimo de 10 caracteres";
-            detail.classList.remove('is-valid');
+           case $('name').value.length < 5:
+               $('name').classList.add('is-invalid')
+               $('error-name').innerText = "El nombre debe tener un mínimo de 5 caracteres";
+               $('name').classList.remove('is-valid');
+   
+               break
+           default:
+               $('name').classList.remove('is-invalid');
+               $('name').classList.add('is-valid');
+               $('error-name').innerText = "";
+       }
+   
+   });
+
+   $('code').addEventListener('keypress', () => {
+    switch (true) {
+           case $('code').value.length < 4:
+               $('code').classList.add('is-invalid')
+               $('error-code').innerText = "El nombre debe tener un mínimo de 4 numeros";
+               $('code').classList.remove('is-valid');
+   
+               break
+           default:
+               $('code').classList.remove('is-invalid');
+               $('code').classList.add('is-valid');
+               $('error-code').innerText = "";
+       }
+   
+   });
+
+
+$('detail').addEventListener('keypress', () => {
+    switch (true) {
+        case $('detail').value.length < 20:
+            $('detail').classList.add('is-invalid')
+            $('error-detail').innerText = "El detalle debe tener un mínimo de 20 caracteres";
+            $('detail').classList.remove('is-valid');
 
             break
         default:
-            detail.classList.remove('is-invalid');
-            detail.classList.add('is-valid');
+            $('detail').classList.remove('is-invalid');
+            $('detail').classList.add('is-valid');
             $('error-detail').innerText = "";
     }
 
 })
+
+$('price').addEventListener('keypress', () => {
+    switch (true) {
+           case $('price').value.length < 4:
+               $('price').classList.add('is-invalid')
+               $('error-price').innerText = "El nombre debe tener un mínimo de 4 numeros";
+               $('price').classList.remove('is-valid');
+   
+               break
+           default:
+               $('price').classList.remove('is-invalid');
+               $('price').classList.add('is-valid');
+               $('error-price').innerText = "";
+       }
+   
+   });
+
+
+
+
