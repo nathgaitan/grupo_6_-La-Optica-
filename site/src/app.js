30222c554b3ec5e4ec2910ff2c2bbc6f7a649ec1
kevinSarli <kevin.sarli99@gmail.com>
2021-11-06 02:49:20 -0300

    ruta de apis

--- site/src/app.js
+++ site/src/app.js
@@ -49,6 +49,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/admin', sessionUserAdmin, adminRouter);
+app.use('/api',require('./routes/apis'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
